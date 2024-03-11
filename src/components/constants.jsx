a2341bfe8e3438ae369eae17a35c14cc47f5e940
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-11 15:17:06 +0530

    update

---
+++ src/components/constants.jsx
@@ -0,0 +1,59 @@
+export const categoriesList = [
+  "Mobile & Laptops",
+  "Gaming Entertainment",
+  "Image & Video",
+  "Vehicles",
+  "Furnitures",
+  "Sport",
+  "Food & Drinks",
+  "Fashion Accessories",
+  "Toilet & Sanitation",
+  "Makeup Corner",
+  "Baby Items",
+];
+
+export const submenuItems = [
+  { text: "Home Two" },
+  { text: "Home Three" },
+  { text: "Home Four" },
+];
+
+export const submenuItemsForShop = [
+  { text: "Privacy Policy" },
+  { text: "FAQ" },
+  { text: "Terms and Conditions" },
+];
+
+export const homePageMenuItems = [
+  { name: "Home One" },
+  { name: "Home Two" },
+  { name: "Home Three" },
+  { name: "Home Four" },
+  { name: "Home Five" },
+];
+export const shopListMenuItems = [
+  { name: "Shop Sidebar" },
+  { name: "Shop Fullwidth" },
+  { name: "Shop Category Icon" },
+  { name: "Shop Category Icon" },
+  { name: "Shop List View" },
+];
+export const productLayoutsMenuItems = [
+  { name: "Horizonral Thumbnail" },
+  { name: "Vertical Thumbnail" },
+  { name: "Gallery Thumbnail" },
+  { name: "Sticky Summary" },
+];
+export const popularCategoriesMenuItems = [
+  { name: "Phone & Tablet" },
+  { name: "Gaming & Sports" },
+  { name: "Home Appliance" },
+  { name: "Fashion Clothes" },
+];
+export const pagesMenuItems = [
+  { name: "Privacy Policy" },
+  { name: "Terms and Conditions" },
+  { name: "FAQ" },
+  { name: "Shop Category Icon" },
+  { name: "Shop List View" },
+];
