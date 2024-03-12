3c3ae7a623994d4f46128812d2f8befcd5ab9166
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-12 12:58:50 +0530

    added extra cards inide the SectionStyleFour

--- src/components/SectionStyleFour.jsx
+++ src/components/SectionStyleFour.jsx
@@ -14,7 +14,7 @@ export default function SectionStyleFour({
         <div className="products-section w-full">
           <div className="grid lg:grid-cols-3 grid-cols-1 xl:gap-[30px] lg:gap-5">
             <div className="item-col">
-              <DataIteration datas={products} startLength={0} endLength={4}>
+              <DataIteration datas={products} startLength={0} endLength={10}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
