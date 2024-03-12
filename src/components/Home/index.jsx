f024993a03ad2af8e051aba5bb9938f6a540b1f8
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-12 15:07:45 +0530

    Banner is swapped with productAds

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -13,7 +13,14 @@ export default function HomeTwo() {
   const { products } = datas;
   return (
     <Layout>
-      <Banner className="banner-wrapper mb-[46px]" />
+      <ProductsAds
+        ads={[
+          `${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-2.2.png`,
+          `${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-2.1.png`,
+        ]}
+        sectionHeight="sm:h-[290px] h-full"
+        className="products-ads-section mb-[60px]"
+      />
       <ViewMoreTitle
         className="my-categories mb-[60px]"
         seeMoreUrl="/all-products"
@@ -29,14 +36,8 @@ export default function HomeTwo() {
         className="new-products mb-[60px]"
       />
       <CampaignCountDown className="mb-[60px]" lastDate="2023-10-04 4:00:00" />
-      <ProductsAds
-        ads={[
-          `${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-2.2.png`,
-          `${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-2.1.png`,
-        ]}
-        sectionHeight="sm:h-[290px] h-full"
-        className="products-ads-section mb-[60px]"
-      />
+
+      <Banner className="banner-wrapper mb-[46px]" />
       <SectionStyleThreeHomeTwo
         products={products.slice(3, 7)}
         showProducts={3}
