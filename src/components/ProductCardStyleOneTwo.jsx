080a56beed089e170983c27691f3f859233e1c3d
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-14 14:51:34 +0530

    New Arrivals cart and svg functionality is removed

--- src/components/ProductCardStyleOneTwo.jsx
+++ src/components/ProductCardStyleOneTwo.jsx
@@ -19,13 +19,6 @@ export default function ProductCardStyleOneTwo({ datas }) {
       ></div>
       <div className="product-card-details flex justify-center h-[102px] items-center  relative">
         {/* add to card button */}
-        <div className="absolute w-[204px] h-[54px] left-[80px] -bottom-20 group-hover:bottom-[65px] transition-all duration-300 ease-in-out">
-          <button type="button" className="yellow-btn">
-            <div>
-              <span>Add To Cart</span>
-            </div>
-          </button>
-        </div>
         <div>
           <a to="/single-product">
             <p className="title mb-2.5 text-[20px] font-600 text-center text-qblack leading-[24px] line-clamp-2 hover:text-blue-600">
@@ -43,24 +36,6 @@ export default function ProductCardStyleOneTwo({ datas }) {
             </div>
           </div>
         </div>
-      </div>
-      {/* quick-access-btns */}
-      <div className="quick-access-btns flex flex-col space-y-2 absolute group-hover:right-[50px] -right-[50px] top-20  transition-all duration-300 ease-in-out">
-        <a href="#">
-          <span className="w-10 h-10 flex justify-center items-center bg-[#CCECEB] rounded">
-            <QuickViewIco />
-          </span>
-        </a>
-        <a href="#">
-          <span className="w-10 h-10 flex justify-center items-center bg-[#CCECEB] rounded">
-            <ThinLove />
-          </span>
-        </a>
-        <a href="#">
-          <span className="w-10 h-10 flex justify-center items-center bg-[#CCECEB] rounded">
-            <Compair />
-          </span>
-        </a>
       </div>
     </div>
   );
