3cd7e5d66c9daf23a0abfa4d952610e0292bae56
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-14 19:02:07 +0530

    product-card-img w-full h-[500px] mt-4 in Popular Sales is resized

--- src/components/ProductCardStyleOneTwo.jsx
+++ src/components/ProductCardStyleOneTwo.jsx
@@ -10,7 +10,7 @@ export default function ProductCardStyleOneTwo({ datas }) {
       style={{ boxShadow: "0px 15px 64px 0px rgba(0, 0, 0, 0.05)" }}
     >
       <div
-        className="product-card-img w-full h-[322px] mt-4"
+        className="product-card-img w-full h-[500px] mt-4"
         style={{
           background: `url(${
             import.meta.env.VITE_PUBLIC_URL
