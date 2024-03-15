96daaafd50e704a764b7d09fdde246ac69b59d6c
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-15 14:45:55 +0530

    Topbar is swapped with Middlebar

--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -6,7 +6,6 @@ import TopBar from "./TopBar";
 export default function HeaderTwo({ className, drawerAction }) {
   return (
     <header className={` ${className || ""} header-section-wrapper relative`}>
-      <TopBar className="quomodo-shop-top-bar" />
       <Middlebar className="quomodo-shop-middle-bar lg:block hidden" />
       <div className="quomodo-shop-drawer lg:hidden block w-full h-[60px] bg-white">
         <div className="w-full h-full flex justify-between items-center px-5">
@@ -50,6 +49,8 @@ export default function HeaderTwo({ className, drawerAction }) {
           </div>
         </div>
       </div>
+      <TopBar className="quomodo-shop-top-bar" />
+
       <Navbar className="quomodo-shop-nav-bar lg:block hidden" />
     </header>
   );
