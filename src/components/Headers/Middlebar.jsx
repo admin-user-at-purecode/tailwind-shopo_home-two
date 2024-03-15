98f95538a00a9b447f47bc3ae6771d9beeab4df4
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-15 16:06:56 +0530

    added extra items insid Middlebar

--- src/components/Headers/Middlebar.jsx
+++ src/components/Headers/Middlebar.jsx
@@ -73,6 +73,48 @@ export default function Middlebar({ className }) {
                   </span>
                 </a>
               </div>
+              <div className="compaire relative">
+                <a to="/products-compaire">
+                  <span>
+                    <Compair />
+                  </span>
+                </a>
+                <span className="w-[18px] h-[18px] rounded-full bg-qh2-green absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-white">
+                  2
+                </span>
+              </div>
+              <div className="favorite relative">
+                <a to="/wishlist">
+                  <span>
+                    <ThinLove />
+                  </span>
+                </a>
+                <span className="w-[18px] h-[18px] rounded-full bg-qh2-green absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-white">
+                  1
+                </span>
+              </div>
+              <div className="cart-wrapper group relative py-4">
+                <div className="cart relative cursor-pointer">
+                  <a to="/cart">
+                    <span>
+                      <ThinBag />
+                    </span>
+                  </a>
+                  <span className="w-[18px] h-[18px] rounded-full bg-qh2-green absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-white">
+                    15
+                  </span>
+                </div>
+                {/* <div className="fixed left-0 top-0 w-full h-full z-40"></div> */}
+                {/* hidden group-hover:block" */}
+                <Cart className="absolute -right-[45px] top-11 z-50 hidden group-hover:block" />
+              </div>
+              <div>
+                <a to="/profile">
+                  <span>
+                    <ThinPeople />
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
