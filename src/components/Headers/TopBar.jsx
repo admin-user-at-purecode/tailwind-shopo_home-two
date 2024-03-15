ddf5e453d306b132d4a2c35171edd28fe6d1a846
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-15 15:53:47 +0530

    added extra topbar-dropdowns sm:block hidden inside topbar

--- src/components/Headers/TopBar.jsx
+++ src/components/Headers/TopBar.jsx
@@ -68,6 +68,38 @@ export default function TopBar({ className }) {
                 </div>
               </div>
             </div>
+            <div className="topbar-dropdowns sm:block hidden">
+              <div className="flex space-x-6">
+                <div className="country-select flex space-x-1 items-center">
+                  <div>
+                    <img
+                      src={`${
+                        import.meta.env.VITE_PUBLIC_URL
+                      }/src/assets/images/country-logo-16x16.png`}
+                      width="16"
+                      height="16"
+                      alt="country logo"
+                      className="overflow-hidden rounded-full"
+                    />
+                  </div>
+                  <Selectbox
+                    className="w-fit"
+                    datas={["United State", "Bangladesh", "India"]}
+                  />
+                  <div>
+                    <Arrow className="fill-current qblack" />
+                  </div>
+                </div>
+                <div className="currency-select flex space-x-1 items-center">
+                  <Selectbox className="w-fit" datas={["USD", "BDT"]} />
+                  <Arrow className="fill-current qblack" />
+                </div>
+                <div className="language-select flex space-x-1 items-center">
+                  <Selectbox className="w-fit" datas={["Bangla", "english"]} />
+                  <Arrow className="fill-current qblack" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
